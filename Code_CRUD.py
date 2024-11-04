76adc7e7f1eaccce0ffd574aec87bb80986a9a71
Trịnh Lệ Nhung <102423194+nhung179@users.noreply.github.com>
2024-11-04 15:17:33 +0700

    Update Code_CRUD.py

--- Code_CRUD.py
+++ Code_CRUD.py
@@ -1,6 +1,5 @@
 import requests
 import json
-
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,7 +14,6 @@ class PfSense:
     def login(self):
         login_url = f"https://{self.host}/index.php"
         response = self.session.get(login_url, auth=(self.username, self.password))
-        print(response.status_code)
         if response.status_code == 200:
             return True
         else:
@@ -24,7 +22,6 @@ class PfSense:
     def read_rules(self):
         read_url = f"https://{self.host}/api/v2/firewall/rules" 
         response = self.session.get(read_url, auth=(self.username, self.password))
-
         if response.status_code == 200:
             rules = response.json()
             return rules
@@ -62,22 +59,14 @@ class PfSense:
             "direction": "any"
         }
         response = self.session.post(create_url, json=rule_data, auth=(self.username, self.password))
-        print(response.status_code)
-        print(response.text)
-
         return response.status_code == 200
            
     def delete_rule(self, rule_id):
         delete_url = f"https://{self.host}/api/v2/firewall/rule/?id={rule_id}"
         response = self.session.delete(delete_url, auth=(self.username, self.password))
-        print(response.status_code)
-        print(response.text)
-
         if response.status_code == 200:
             return True
         else:
-            print(f"Failed to delete rule with ID {rule_id}. Error code: {response.status_code}")
-            print(response.text)
             return False
         
     def update_rule(self):
@@ -110,8 +99,6 @@ class PfSense:
             "direction": "any"
         }
         response = self.session.patch(update_url, json=rule_data, auth=(self.username, self.password))
-        print(response.status_code)
-        print(response.text)
         return response.status_code == 200
 
     def replace_rule(self):
@@ -144,17 +131,13 @@ class PfSense:
             "quick": None,
             "direction": "any"
         }]
-
         response = self.session.put(replace_url, json=rule_data, auth=(self.username, self.password))
-        print(response.status_code)
-        print(response.text)
         return response.status_code == 200
     
 if __name__ == "__main__":
     HOST = ""
     USERNAME = ""
     PASSWORD = ""
-
     pfsense = PfSense(HOST, USERNAME, PASSWORD)
 
     if pfsense.login():
@@ -162,27 +145,22 @@ if __name__ == "__main__":
         rules = pfsense.read_rules()
         if rules:
             print(json.dumps(rules, indent=4))
-
         if pfsense.create_rule():
             print("Rule created successfully.")
         else:
             print("Failed to create rule")
-        
         # rule_id = input("Enter the rule ID to delete: ")
         # if pfsense.delete_rule(rule_id):
         #     print(f"Rule with ID {rule_id} deleted successfully.")
         # else:
         #     print(f"Failed to delete rule with ID {rule_id}.")
-
         if pfsense.update_rule():
             print("Rule updated successfully.")
         else:
             print("Failed to update rule")
-
         # if pfsense.replace_rule():
         #     print("Rule replaced successfully.")
         # else:
         #     print("Failed to replace rule")
-
     else:
         print("Failed login")
